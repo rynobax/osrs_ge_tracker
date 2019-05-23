155c0ae073c2e6fc2ee92313e3c23a5ab8160b02
Ryan Baxley <ryan.baxley1@gmail.com>
2019-05-22 21:20:00 -0400

    Implement getCurrentPrices

--- lib/osrs_ge_tracker/osbuddy/osbuddy.ex
+++ lib/osrs_ge_tracker/osbuddy/osbuddy.ex
@@ -4,4 +4,22 @@ defmodule OsrsGeTracker.OSBuddy do
     %HTTPoison.Response{body: body} = HTTPoison.get!(url)
     Poison.decode!(body) |> Enum.map(fn {k, v} -> %{id: k, name: v["name"]} end)
   end
+
+  def getCurrentPrices do
+    url = "https://storage.googleapis.com/osbuddy-exchange/summary.json"
+    %HTTPoison.Response{body: body} = HTTPoison.get!(url)
+
+    Poison.decode!(body)
+    |> Enum.map(fn {k, v} ->
+      %{
+        item_id: k,
+        buy_avg: v["buy_average"],
+        buy_qty: v["buy_quantity"],
+        overall_avg: v["overall_average"],
+        overall_qty: v["overall_quantity"],
+        sell_avg: v["sell_average"],
+        sell_qty: v["sell_quantity"]
+      }
+    end)
+  end
 end
